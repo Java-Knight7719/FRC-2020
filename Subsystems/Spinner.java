9a574d713f45745178110bcc940378605211f784
MNRF <45114806+MNRF@users.noreply.github.com>
2020-02-12 15:31:58 +0700

    Update Spinner.java

--- Subsystems/Spinner.java
+++ Subsystems/Spinner.java
@@ -1,1 +1,66 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
+package frc.robot.subsystems;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.commands.Spin;
+
+public class Spinner extends Subsystem {
+
+  public final I2C.Port i2cPort = I2C.Port.kOnboard;
+
+  /**
+   * A Rev Color Sensor V3 object is constructed with an I2C port as a 
+   * parameter. The device will be automatically initialized with default 
+   * parameters.
+   */
+  public final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+
+  /**
+   * A Rev Color Match object is used to register and detect known colors. This can 
+   * be calibrated ahead of time or during operation.
+   * 
+   * This object uses a simple euclidian distance to estimate the closest match
+   * with given confidence range.
+   */
+  public final ColorMatch m_colorMatcher = new ColorMatch();
+
+  /**
+   * Note: Any example colors should be calibrated as the user needs, these
+   * are here as a basic example.
+   */
+  public final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  public final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  public final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  public final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
+  public Color detectedColor;
+  public String colorString;
+  public ColorMatchResult match;
+  public double counter;
+  public String prevcolor;
+  public String colorwarna[] = {"Blue", "Green", "Red", "Yellow"};
+
+  @Override
+  public void initDefaultCommand() {
+
+    setDefaultCommand(new Spin());
+
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);   
+
+  }
+}
