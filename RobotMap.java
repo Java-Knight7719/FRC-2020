5f65522e43a4bd7926d22ca18324174d56fe1a66
MNRF <45114806+MNRF@users.noreply.github.com>
2020-02-12 15:51:16 +0700

    Create RobotMap.java

---
+++ RobotMap.java
@@ -0,0 +1,79 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+public class RobotMap {
+  
+  //MOTOR CONTROLLER CAN ID
+  
+  public static final int m1_neo = 1;
+  public static final int m2_neo = 2;
+	public static final int m3_neo = 3;
+  public static final int m4_neo = 4;
+  public static final int m5_neo = 5;
+  public static final int m6_neo = 6;
+  public static final int m7_neo = 7;
+
+  
+
+
+  //MOTOR CONTROLLER PWM ID
+
+  public static final int m1_ = 0;
+
+
+  //Controller USB Port
+
+  public static final int P1F310 = 0;
+  public static final int P2F310 = 1;
+  
+
+  //F310 Player 1 Axis
+
+  public static final int P1LeftX = 0;
+  public static final int P1LeftY = 1;
+  public static final int P1LeftTrig = 2;
+  public static final int P1RightTrig = 3;
+  public static final int P1RightX = 4;
+  public static final int P1RightY = 5;
+
+
+  //F310 Player 2 Axis
+
+  public static final int P2LeftX = 0;
+  public static final int P2LeftY = 1;
+  public static final int P2LeftTrig = 2;
+  public static final int P2RightTrig = 3;
+  public static final int P2RightX = 4;
+  public static final int P2RightY = 5;
+
+
+  //F310 Player 1 Buttons
+
+  public static final int P1A = 1;
+  public static final int P1B = 2;
+  public static final int P1X = 3;
+  public static final int P1Y = 4;
+  public static final int P1LBumper = 5;
+  public static final int P1RBumper = 6;
+  public static final int P1Back = 7;
+  public static final int P1Start = 8;
+
+
+  //F310 Player 2 Buttons
+
+  public static final int P2A = 1;
+  public static final int P2B = 2;
+  public static final int P2X = 3;
+  public static final int P2Y = 4;
+  public static final int P2LBumper = 5;
+  public static final int P2RBumper = 6;
+  public static final int P2Back = 7;
+  public static final int P2Start = 8;
+
+}
